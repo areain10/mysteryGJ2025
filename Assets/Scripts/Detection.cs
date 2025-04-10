311a458aa308c8ffe2b23fa931366437b86af188
r-meadows <rymeadow@uci.edu>
2025-04-09 18:16:19 -0700

    added more detection
    material still not changing color for some reason

--- Assets/Scripts/Detection.cs
+++ Assets/Scripts/Detection.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class Detection : MonoBehaviour
 {
-    void Update()
-        {
-            if (Input.GetMouseButtonDown(0)) 
-            {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
-                RaycastHit hit;
+    [SerializeField] private Color startColor;
+    [SerializeField] private Renderer renderer;
 
-                if (Physics.Raycast(ray, out hit)) 
-                {
-                    if (hit.collider.gameObject.CompareTag("Interactable"))
-                    {
-                        Debug.Log("Interacted with: " + hit.collider.gameObject.name);
-                    }
-                }
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+
+        var col = renderer.material.color;
+        col.a = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.CompareTag("Interactable")) 
+            {
+                Debug.Log("Interacted with: " + hit.collider.gameObject.name);
             }
         }
+    }
+
+    void OnMouseEnter ()
+    {
+        startColor = renderer.material.color;
+        renderer.material.color = Color.red;
+    }
+
+    void OnMouseExit ()
+    {
+        renderer.material.color = startColor;
+    }
 }
