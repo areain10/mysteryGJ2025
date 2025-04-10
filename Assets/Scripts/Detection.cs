474fe3668228245e2e1389703c94e544837fd89c
r-meadows <rymeadow@uci.edu>
2025-04-09 22:54:53 -0700

    adjusted object interaction

--- Assets/Scripts/Detection.cs
+++ Assets/Scripts/Detection.cs
@@ -7,21 +7,39 @@ public class Detection : MonoBehaviour
 {
     [SerializeField] private Color startColor;
     [SerializeField] private Renderer renderer;
-    
+
     [SerializeField] private GameObject itemPopup;
     private TextMeshProUGUI itemDesc;
-    [SerializeField] private string description;
+
+    [TextArea(2, 5)]
+    [SerializeField] private string[] firstInteractionText;
+
+    [TextArea(2, 5)]
+    [SerializeField] private string[] followUpText;
+
+    [TextArea(2, 5)]
+    [SerializeField] private string interactionPromptText;
+
+    private int currentLine = 0;
+    private bool isInteracting = false;
+    private bool seen = false; 
+
+    private string[] currentText;
+    private bool isPromptActive = false;
 
     void Start()
     {
-        renderer = GetComponent<Renderer>();
-        
-        itemDesc = itemPopup.GetComponentInChildren<TextMeshProUGUI>();
+        if (renderer == null)
+            renderer = GetComponent<Renderer>();
+
+        if (itemPopup != null)
+        {
+            itemDesc = itemPopup.GetComponentInChildren<TextMeshProUGUI>();
+            itemPopup.SetActive(false);
+        }
 
         var col = renderer.material.color;
         col.a = 1f;
-
-        itemPopup.SetActive(false);
     }
 
     void Update()
@@ -31,27 +49,111 @@ public class Detection : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.CompareTag("Interactable")) 
+            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject) 
             {
                 Debug.Log("Interacted with: " + hit.collider.gameObject.name);
 
-                itemPopup.SetActive(true);
+                if (!seen)
+                {
+                    currentText = firstInteractionText;
+                    currentLine = 0;
+
+                    itemPopup.SetActive(true);
+                    itemDesc.text = currentText[currentLine];
+                    isInteracting = true;
+                }
+                else
+                {
+                    currentText = followUpText;
+                    currentLine = 0;
+
+                    itemPopup.SetActive(true);
+                    itemDesc.text = currentText[currentLine];
+                    isInteracting = true;
+                }
             }
         }
 
-        if (itemPopup.activeSelf)
+        if (isInteracting && itemPopup.activeSelf)
         {
-            itemDesc.text = description;
+            if (isPromptActive)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    InteractWithObject(); 
+                }
+
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    ClosePopup();
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Return))
+                {
+                    Continue(); 
+                }
+
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    ClosePopup();
+                }
+            }
         }
     }
 
-    void OnMouseEnter ()
+    private void Continue()
+    {
+        currentLine++;
+
+        if (currentLine < currentText.Length)
+        {
+            itemDesc.text = currentText[currentLine];
+        }
+        else
+        {
+            if (interactionPromptText.Length > 0)
+            {
+                ActivatePrompt();
+            }
+            else
+            {
+                ClosePopup();
+            }
+        }
+    }
+
+    private void ActivatePrompt()
+    {
+        if (!string.IsNullOrEmpty(interactionPromptText))
+        {
+            itemDesc.text = interactionPromptText;
+        }
+        isPromptActive = true;
+    }
+
+    private void InteractWithObject()
+    {
+        Debug.Log("You have interacted with the object!"); 
+        ClosePopup();
+    }
+
+    private void ClosePopup()
+    {
+        itemPopup.SetActive(false);
+        isInteracting = false;
+        isPromptActive = false;
+        seen = true; 
+    }
+
+    void OnMouseEnter()
     {
         startColor = renderer.material.color;
         renderer.material.color = Color.red;
     }
 
-    void OnMouseExit ()
+    void OnMouseExit()
     {
         renderer.material.color = startColor;
     }
