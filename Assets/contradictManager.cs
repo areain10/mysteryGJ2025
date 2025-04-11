d9b91a954f6e3c7f9af7b40f9b69084a0d287b50
rain athkravisoonthorn <107684386+areain10@users.noreply.github.com>
2025-04-10 18:11:37 -0700

    testing

--- Assets/contradictManager.cs
+++ Assets/contradictManager.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class contradictManager : MonoBehaviour
 {
+    string[] currentClue;
+    string[] currentDiscrepancy;
+
+    public bool checkIfCorrect()
+    {
+        bool tmp = true;
+        for (int i = 0; i < currentClue.Length; i++)
+        {
+            if (currentClue[i] != currentDiscrepancy[i])
+            {
+                tmp = false;
+            }
+        }
+        return tmp;
+    }
     // Start is called before the first frame update
     void Start()
     {
